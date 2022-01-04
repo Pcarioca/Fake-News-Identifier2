42c639b44b1787449ccbb12ca2f05f6bf9d664e4
Andrei Munteanu <63117204+Pcarioca@users.noreply.github.com>
2022-01-04 10:27:54 +0200

    generating footer dynamically

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,20 @@
 let audioElement1 = document.createElement('audio');
-    audioElement1.setAttribute('id', 'play');
-    audioElement1.setAttribute('src', 'audio/f5.mp3'); 
+audioElement1.setAttribute('id', 'play');
+audioElement1.setAttribute('src', 'audio/f5.mp3');
 
-    $.get();
-    $("#body").append(audioElement1);
+// $.get();
+$("#body").append(audioElement1);
 
 
-    $("a").mouseenter(function() {
-      audioElement1.play();
-      });
+$("a").mouseenter(function () {
+  audioElement1.play();
+});
+
+
+// document.querySelector("body").addEventListener
+// window.addEventListener('load', () => {
+  let foot = '<footer class="text-center text-white" style = "background-color: #ffffff2f;" ><div class="container pt-4"></div><div class="text-center text-white p-3" style="background-color: rgba(255, 255, 255, 0.2);">This Website was created by Munteanu Paul Andrei for <a target="_blank"href="https://infoeducatie.ro/"> InfoEducatie </a></div> </footer>';
+  // footHTML = document.createTextNode(foot)
+  $("#footer").append(foot);
+  console.log("done!");
+// });
