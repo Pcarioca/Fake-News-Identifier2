0b86d3bb5230feda6a716106fe97400a03f96be1
Andrei Munteanu <63117204+Pcarioca@users.noreply.github.com>
2022-03-01 11:49:02 +0200

    button shows up correctly, it does nothing
    few errors regarding firstElementChild from JQ, even if it works fine

--- assets/popup.js
+++ assets/popup.js
@@ -11,29 +11,39 @@ var i = 1;
 var j;
 var text = "slide-";
 var textoOnPage = text + i;
-element = document.getElementById(textoOnPage)//.children[0];
+element = document.getElementById(textoOnPage).firstElementChild.firstElementChild;
+
 
 while(element!=undefined){
-  element.innerHTML += '<button id="myBtn" class="myBTN" >Open Modal</button>';
+  element.innerHTML += '<br> <button class="myBTN" >more details</button>';
   i++;
   var textoOnPage = text + i;
-  element = document.getElementById(textoOnPage)//.children[0];
+  element = document.getElementById(textoOnPage).firstElementChild.firstElementChild;//.children[0];
 }
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementsByClassName("myBTN")[0];
+var btn = document.getElementsByClassName("myBTN");
+
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 
-var buttons = document.getElementsByTagName('button');
-for (var i = 0, len = buttons.length; i < len; i++) {
-    buttons[i].onclick = function (){
-        //myFunction (this);
-        modal.style.display = "block";
+// var buttons = document.getElementsByTagName('button');
+// for (var i = 0, len = buttons.length; i < len; i++) {
+//     buttons[i].onclick = function (){
+//         //myFunction (this);
+//         modal.style.display = "block";
+//     }
+// }
+
+var btns = document.getElementsByClassName("myBTN");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+			    modal.style.display = "block";
+        });
     }
-}
+
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
